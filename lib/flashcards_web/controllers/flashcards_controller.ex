61d2ec915f7c4c8b271d88169f100d2ee99b22d5
Tom Simmons (Tomboyo) <tomasimmons@gmail.com>
2018-12-16 13:17:55 -0500

    Create rough index with stubbed data

--- lib/flashcards_web/controllers/flashcards_controller.ex
+++ lib/flashcards_web/controllers/flashcards_controller.ex
@@ -2,6 +2,13 @@ defmodule FlashcardsWeb.FlashcardsController do
   use FlashcardsWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    cards = get_cards()
+    render(conn, "index.html", [cards: cards])
+  end
+
+  def get_cards() do
+    for id <- 1..10,
+        title = "Card #{id}",
+        do: {id, title, "Card Content"}
   end
 end
--- lib/flashcards_web/views/flashcards_view.ex
+++ lib/flashcards_web/views/flashcards_view.ex
@@ -1,3 +1,7 @@
 defmodule FlashcardsWeb.FlashcardsView do
   use FlashcardsWeb, :view
+
+  def render_card({id, title, content}) do
+    render("card.html", [id: id, title: title, content: content])
+  end
 end
