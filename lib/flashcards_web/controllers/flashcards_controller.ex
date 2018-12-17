7318beedf8eba27e82819fa32629975285cb8fdf
Tom Simmons (Tomboyo) <tomasimmons@gmail.com>
2018-12-16 21:27:14 -0500

    Basic edit functionality

--- lib/flashcards_web/controllers/flashcards_controller.ex
+++ lib/flashcards_web/controllers/flashcards_controller.ex
@@ -4,18 +4,45 @@ defmodule FlashcardsWeb.FlashcardsController do
 
   def index(conn, _params) do
     cards = get_cards()
-    render(conn, "index.html", [cards: cards])
+    render(conn, "index.html", [conn: conn, cards: cards])
   end
 
   defp get_cards() do
     Repo.all(Card)
   end
 
-  def new(conn, params) do
+  def new(conn, _params) do
     conn = case Repo.insert(%Card{}) do
-      {:ok, card} -> conn
+      {:ok, _card} -> conn
       _ -> put_flash(conn, :error, "Could not create flashcard :(")
     end
     redirect(conn, to: Routes.flashcards_path(conn, :index))
+  end
+
+  def edit(conn, %{"id" => id}) do
+    %Card{front: front, back: back} = Repo.get(Card, id)
+    render(conn, "edit.html", [
+      front: front,
+      back: back,
+      id: id,
+      csrf_token: get_csrf_token()
+    ])
+  end
+
+  def show(conn, params) do
+    case params do
+      %{_method: _method} -> redirect conn, to: Routes.flashcards_path(conn, :update)
+      _ -> redirect conn, to: Routes.flashcards_path(conn, :index)
+    end
+  end
+
+  def update(conn, %{"id" => id, "front" => front, "back" => back}) do
+    {id, _} = Integer.parse(id)
+
+    Repo.get(Card, id)
+    |> Card.changeset(%{front: front, back: back})
+    |> Repo.update()
+
+    redirect conn, to: Routes.flashcards_path(conn, :index)
   end
 end
--- lib/flashcards_web/views/flashcards_view.ex
+++ lib/flashcards_web/views/flashcards_view.ex
@@ -2,7 +2,11 @@ defmodule FlashcardsWeb.FlashcardsView do
   use FlashcardsWeb, :view
   alias Flashcards.Card
 
-  def render_card(%Card{front: front, back: back}) do
-    render("card.html", [front: front, back: back])
+  def render_card(conn, %Card{id: id, front: front, back: back}) do
+    render("card.html", [
+      front: front,
+      back: back,
+      edit_path: Routes.flashcards_path(conn, :edit, id)
+    ])
   end
 end
